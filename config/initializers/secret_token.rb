ad3aa6fae15512d9461f2fee2870c943e39733f5
Larry NeJame <lnejame@nebids.com>
2012-07-02 16:06:24 -0400

    simp update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simp::Application.config.secret_token = 'dd7eae0c6e674d0683fa227635c77ca0726d1c9bdbb56b71de38d94ebf75dbaf5b042833841cbbd2dc17f6986263026e981fd239de8e2f3c917a240248b21219'
